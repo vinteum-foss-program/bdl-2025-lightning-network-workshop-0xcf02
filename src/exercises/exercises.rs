b0f38560a87b3c5b23a72b20bc2985c770d3a6ff
Replit user <>
2025-12-30 19:48:31 +0000

    Lightning Network Workshop: done

--- src/exercises/exercises.rs
+++ src/exercises/exercises.rs
@@ -6,27 +6,35 @@
     non_snake_case
 )]
 use crate::internal;
-use bitcoin::script::{Builder, ScriptBuf, ScriptHash};
-use internal::key_utils::{add_pubkeys, pubkey_multipication_tweak, pubkey_from_secret, add_privkeys, privkey_multipication_tweak, hash_pubkeys};
-use internal::tx_utils::{build_output, build_transaction};
-use internal::script_utils::{build_htlc_offerer_witness_script, p2wpkh_output_script};
-use bitcoin::blockdata::opcodes::all as opcodes;
-use bitcoin::secp256k1::{SecretKey, PublicKey as secp256k1PublicKey, Scalar};
-use bitcoin::PublicKey;
+use bitcoin::opcodes::all as opcodes;
 use bitcoin::hashes::Hash;
-use bitcoin::{Block, OutPoint, PubkeyHash, Sequence, Transaction, TxIn, TxOut, Witness};
-use bitcoin::transaction::Version;
 use bitcoin::locktime::absolute::LockTime;
+use bitcoin::script::{Builder, ScriptBuf, ScriptHash};
+use bitcoin::secp256k1::{PublicKey as secp256k1PublicKey, Scalar, SecretKey};
+use bitcoin::transaction::Version;
+use bitcoin::PublicKey;
+use bitcoin::{Block, OutPoint, PubkeyHash, Sequence, Transaction, TxIn, TxOut, Witness};
+use internal::key_utils::{
+    add_privkeys, add_pubkeys, hash_pubkeys, privkey_multipication_tweak, pubkey_from_secret,
+    pubkey_multipication_tweak,
+};
+use internal::script_utils::{build_htlc_offerer_witness_script, p2wpkh_output_script};
+use internal::tx_utils::{build_output, build_transaction};
 
 //
 // Exercise 1
 //
 
-pub fn two_of_two_multisig_witness_script(
-    pubkey1: &PublicKey,
-    pubkey2: &PublicKey,
-) -> ScriptBuf {
-    unimplemented!()
+pub fn two_of_two_multisig_witness_script(pubkey1: &PublicKey, pubkey2: &PublicKey) -> ScriptBuf {
+    let keys = [*pubkey1, *pubkey2];
+
+    bitcoin::script::Builder::new()
+        .push_int(2)
+        .push_key(&keys[0])
+        .push_key(&keys[1])
+        .push_int(2)
+        .push_opcode(opcodes::OP_CHECKMULTISIG)
+        .into_script()
 }
 
 //
@@ -39,8 +47,10 @@ pub fn build_funding_transaction(
     bob_pubkey: &PublicKey,
     amount: u64,
 ) -> Transaction {
-
-    unimplemented!()
+    let witness_script = two_of_two_multisig_witness_script(alice_pubkey, bob_pubkey);
+    let p2wsh_script_pubkey = ScriptBuf::new_p2wsh(&witness_script.wscript_hash());
+    let output = build_output(amount, p2wsh_script_pubkey);
+    build_transaction(Version::TWO, LockTime::ZERO, txins, vec![output])
 }
 
 //
@@ -52,10 +62,20 @@ pub fn build_refund_transaction(
     alice_pubkey: PublicKey,
     bob_pubkey: PublicKey,
     alice_balance: u64,
-    bob_balance: u64
+    bob_balance: u64,
 ) -> Transaction {
+    let alice_script = p2wpkh_output_script(alice_pubkey);
+    let bob_script = p2wpkh_output_script(bob_pubkey);
 
-    unimplemented!()
+    let alice_output = build_output(alice_balance, alice_script);
+    let bob_output = build_output(bob_balance, bob_script);
+
+    let mut outputs = vec![alice_output, bob_output];
+    outputs.sort_by(|a, b| {
+        a.value.cmp(&b.value).then_with(|| a.script_pubkey.cmp(&b.script_pubkey))
+    });
+
+    build_transaction(Version::TWO, LockTime::ZERO, vec![funding_txin], outputs)
 }
 
 //
@@ -66,15 +86,33 @@ pub fn generate_revocation_pubkey(
     countersignatory_basepoint: secp256k1PublicKey,
     per_commitment_point: secp256k1PublicKey,
 ) -> secp256k1PublicKey {
-    unimplemented!()
+    let tweak1 = hash_pubkeys(countersignatory_basepoint, per_commitment_point);
+    let tweak2 = hash_pubkeys(per_commitment_point, countersignatory_basepoint);
+
+    let p1 = pubkey_multipication_tweak(countersignatory_basepoint, tweak1);
+    let p2 = pubkey_multipication_tweak(per_commitment_point, tweak2);
+
+    add_pubkeys(p1, p2)
 }
 
 //
 // Exercise 5
 //
 
-pub fn generate_revocation_privkey(countersignatory_per_commitment_secret: SecretKey, revocation_base_secret: SecretKey) -> SecretKey {
-    unimplemented!()
+pub fn generate_revocation_privkey(
+    countersignatory_per_commitment_secret: SecretKey,
+    revocation_base_secret: SecretKey,
+) -> SecretKey {
+    let base_point = pubkey_from_secret(revocation_base_secret);
+    let per_commitment_point = pubkey_from_secret(countersignatory_per_commitment_secret);
+
+    let tweak1 = hash_pubkeys(base_point, per_commitment_point);
+    let tweak2 = hash_pubkeys(per_commitment_point, base_point);
+
+    let sk1 = privkey_multipication_tweak(revocation_base_secret, tweak1);
+    let sk2 = privkey_multipication_tweak(countersignatory_per_commitment_secret, tweak2);
+
+    add_privkeys(sk1, sk2)
 }
 
 //
@@ -86,7 +124,17 @@ pub fn to_local(
     to_local_delayed_pubkey: &PublicKey,
     to_self_delay: i64,
 ) -> ScriptBuf {
-    unimplemented!()
+    Builder::new()
+        .push_opcode(opcodes::OP_IF)
+        .push_key(revocation_key)
+        .push_opcode(opcodes::OP_ELSE)
+        .push_int(to_self_delay)
+        .push_opcode(opcodes::OP_CSV)
+        .push_opcode(opcodes::OP_DROP)
+        .push_key(to_local_delayed_pubkey)
+        .push_opcode(opcodes::OP_ENDIF)
+        .push_opcode(opcodes::OP_CHECKSIG)
+        .into_script()
 }
 
 //
@@ -102,8 +150,19 @@ pub fn build_commitment_transaction(
     local_amount: u64,
     remote_amount: u64,
 ) -> Transaction {
+    let to_local_script = to_local(revocation_pubkey, to_local_delayed_pubkey, to_self_delay);
+    let to_local_p2wsh = ScriptBuf::new_p2wsh(&to_local_script.wscript_hash());
+    let local_output = build_output(local_amount, to_local_p2wsh);
 
-    unimplemented!()
+    let remote_script = p2wpkh_output_script(remote_pubkey);
+    let remote_output = build_output(remote_amount, remote_script);
+
+    let mut outputs = vec![local_output, remote_output];
+    outputs.sort_by(|a, b| {
+        a.value.cmp(&b.value).then_with(|| a.script_pubkey.cmp(&b.script_pubkey))
+    });
+
+    build_transaction(Version::TWO, LockTime::ZERO, vec![funding_txin], outputs)
 }
 
 //
@@ -123,7 +182,29 @@ pub fn build_htlc_commitment_transaction(
     local_amount: u64,
     remote_amount: u64,
 ) -> Transaction {
-    unimplemented!()
+    let to_local_script = to_local(revocation_pubkey, to_local_delayed_pubkey, to_self_delay);
+    let to_local_p2wsh = ScriptBuf::new_p2wsh(&to_local_script.wscript_hash());
+    let local_output = build_output(local_amount, to_local_p2wsh);
+
+    let remote_script = p2wpkh_output_script(remote_pubkey);
+    let remote_output = build_output(remote_amount, remote_script);
+
+    let htlc_script = build_htlc_offerer_witness_script(
+        revocation_pubkey, 
+        remote_htlc_pubkey, 
+        local_htlc_pubkey, 
+        payment_hash160
+    );
+
+    let htlc_p2wsh = ScriptBuf::new_p2wsh(&htlc_script.wscript_hash());
+    let htlc_output = build_output(htlc_amount, htlc_p2wsh);
+
+    let mut outputs = vec![local_output, remote_output, htlc_output];
+    outputs.sort_by(|a, b| {
+        a.value.cmp(&b.value).then_with(|| a.script_pubkey.cmp(&b.script_pubkey))
+    });
+
+    build_transaction(Version::TWO, LockTime::ZERO, vec![funding_txin], outputs)
 }
 
 //
@@ -138,5 +219,11 @@ pub fn build_htlc_timeout_transaction(
     cltv_expiry: u32,
     htlc_amount: u64,
 ) -> Transaction {
-    unimplemented!()
+    let to_local_script = to_local(revocation_pubkey, to_local_delayed_pubkey, to_self_delay);
+    let to_local_p2wsh = ScriptBuf::new_p2wsh(&to_local_script.wscript_hash());
+    let output = build_output(htlc_amount, to_local_p2wsh);
+
+    let mut tx = build_transaction(Version::TWO, LockTime::ZERO, vec![htlc_txin], vec![output]);
+    tx.lock_time = LockTime::from_consensus(cltv_expiry);
+    tx
 }
--- submissions/done.sh
+++ submissions/done.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 export LDK_WORKSHOP_RESULT="PASS"
+# https://replit.com/@0xcf02/Programming-Lightning-Intro-to-Payment-Channels-mini-1
