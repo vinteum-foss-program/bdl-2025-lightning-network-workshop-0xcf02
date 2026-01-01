e96338ca9e84231eb9c756926c7efc77cc980380
Replit user <>
2026-01-01 21:01:54 +0000

    fix permissions

--- submissions/done.sh
+++ submissions/done.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LDK_WORKSHOP_RESULT="PASS"
-https://replit.com/@0xcf02/Programming-Lightning-Intro-to-Payment-Channels-mini-1#submissions/done.sh
+https://replit.com/@0xcf02/Programming-Lightning-Intro-to-Payment-Channels-mini-1
