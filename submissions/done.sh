c980c4b55bf71047caba9ea1a9c279aafdaf911c
Replit user <>
2026-01-02 18:57:56 +0000

    fix: permissions

--- submissions/done.sh
+++ submissions/done.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-RESULT="PASS"
 REPLIT_LINK="https://replit.com/@0xcf02/Programming-Lightning-Intro-to-Payment-Channels-mini-1"
-#trying git update-index --chmod=+x submissions/done.sh
+
+echo "PASS"
